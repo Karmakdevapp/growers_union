b5843896275233f0fc5375748dcd7ee5d9439fb7
gastonnrodriguez <gastonnrodriguez@gmail.com>
2022-05-11 22:46:22 -0300

    login listo

---
+++ src/components/Login.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import "antd/dist/antd.css";
+const Login = () => {
+  const onFinish = values => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="loginForm">
+      <img className="logo" src="../assets/logo.png" alt="logo" />
+      <Form
+        className="login"
+        name="basic"
+        labelCol={{
+          span: 10,
+        }}
+        wrapperCol={{
+          span: 4,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 10,
+            span: 4,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 10,
+            span: 4,
+          }}
+        >
+          <Button type="primary" htmlType="submit" block>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
